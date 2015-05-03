fe77b8ac5dd35d9b95f266ad4a43f47804d2fdfd
Ashok Kafle <ashookkafle@gmail.com>
2015-05-03 00:17:59 -0500

    Normal JSF page pattern generated for user table

---
+++ MyCommunity/src/main/java/com/mycommunity/entities/UsersFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycommunity.entities;
+
+import com.mycommunity.entities.Users;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ASHOK
+ */
+@Stateless
+public class UsersFacade extends AbstractFacade<Users> {
+    @PersistenceContext(unitName = "com.mycommunity_MyCommunity_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UsersFacade() {
+        super(Users.class);
+    }
+    
+}
