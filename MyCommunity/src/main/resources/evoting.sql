a6bcc8ea2ab25fe26259b761c977fd1d908f5a52
Ashok Kafle <ashookkafle@gmail.com>
2015-05-02 21:52:19 -0500

    JAAS security login maintained!

---
+++ MyCommunity/src/main/resources/evoting.sql
@@ -0,0 +1,50 @@
+/*
+SQLyog Ultimate v9.50 
+MySQL - 5.6.21 : Database - mycommunity_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mycommunity_db` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `mycommunity_db`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `USERID` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `PASSWORD` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`USERID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`USERID`,`PASSWORD`) values ('admin','19404ce9e897c60dde098e25d85dece4f4a6f36b4cc58d805f582d9cea14e6eb');
+
+/*Table structure for table `users_groups` */
+
+DROP TABLE IF EXISTS `users_groups`;
+
+CREATE TABLE `users_groups` (
+  `GROUPID` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `USERID` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`GROUPID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `users_groups` */
+
+insert  into `users_groups`(`GROUPID`,`USERID`) values ('admin','admin');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
